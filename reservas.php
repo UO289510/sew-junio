c5577e9b7e7d2947374b50fea321515c13c85043
UO289510 <UO289510@uniovi.es>
2025-06-15 21:03:54 +0200

    Add files via upload

--- reservas.php
+++ reservas.php
@@ -18,6 +18,9 @@
     <body>
 
         <?php
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
             session_start();
             require_once "php/recursos.php";
             class Usuario {
